cfd807526fb9557bf265e2f4c34d17c8e305dfa4
OKCoyote <87919973+OKCoyote@users.noreply.github.com>
2021-08-22 18:43:15 -0700

    Update SpeakHello.js
    Changed:
    
    helloSpeaker.speak = function () {
      console.log(helloSpeaker.speakWord + " " + helloSpeaker.names[i]);
    }
    
    To:
    
    helloSpeaker.speak = function () {
      console.log(helloSpeaker.speakWord + " " + name);
    }

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -5,7 +5,7 @@ var helloSpeaker = {};
 helloSpeaker.speakWord = "Hello";
 
 helloSpeaker.speak = function () {
-  console.log(helloSpeaker.speakWord + " " + helloSpeaker.names[i]);
+  console.log(helloSpeaker.speakWord + " " + name);
 }
 
 window.helloSpeaker = helloSpeaker;
